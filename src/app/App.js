0fb3c3ede645a098401b7d70d53cfe4711233775
Fernando Hernandez <fefehern@hotmail.com>
2017-08-29 01:24:20 -0400

    Added Semantic. Still working on implementation

--- src/app/App.js
+++ src/app/App.js
@@ -4,9 +4,12 @@ import '../res/components/App.css';
 import Clock from "../clock/";
 import Link from "react-router-dom/es/Link";
 
+import {Button, Container, Header, Icon, Menu, Segment, Visibility} from 'semantic-ui-react';
+
+
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {users: []};
         this.handleClick = this.handleClick.bind(this);
     }
@@ -14,11 +17,50 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <h1>test</h1>
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
+                    <Segment
+                        inverted
+                        textAlign='center'
+                        style={{ minHeight: 700, padding: '1em 0em' }}
+                        vertical
+                    >
+                        <Container>
+                            <Menu inverted pointing secondary size='large'>
+                                <Menu.Item as='a' active>Home</Menu.Item>
+                                <Menu.Item as='a'>Work</Menu.Item>
+                                <Menu.Item as='a'>Company</Menu.Item>
+                                <Menu.Item as='a'>Careers</Menu.Item>
+                                <Menu.Item position='right'>
+                                    <Button as='a' inverted>Log in</Button>
+                                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                                </Menu.Item>
+                            </Menu>
+                        </Container>
+
+                        <Container text>
+                            <Header
+                                as='h1'
+                                content='Imagine-a-Company'
+                                inverted
+                                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+                            />
+                            <Header
+                                as='h2'
+                                content='Do whatever you want when you want to.'
+                                inverted
+                                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+                            />
+                            <Button primary size='huge'>
+                                Get Started
+                                <Icon name='right arrow' />
+                            </Button>
+                        </Container>
+                        <div className="App-header">
+                            <h1>test</h1>
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <h2>Welcome to React</h2>
+                        </div>
+                    </Segment>
+
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload. <br/>
                     Good morning
--- src/app/Layout.js
+++ src/app/Layout.js
@@ -7,13 +7,48 @@ import Routes from "./Routes";
 import '../res/components/Layout.css';
 //import Link from "react-router-dom/es/Link";
 
+import {
+    Button,
+    Container,
+    Menu,
+    Segment,
+    Visibility
+} from 'semantic-ui-react';
+
+// header
+const FixedMenu = () => (
+    <Menu fixed='top' size='large'>
+        <Container>
+            <Menu.Item as='a' active>Home</Menu.Item>
+            <Menu.Item as='a'>Work</Menu.Item>
+            <Menu.Item as='a'>Company</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item className='item'>
+                    <Button as='a'>Log in</Button>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button as='a' primary>Sign Up</Button>
+                </Menu.Item>
+            </Menu.Menu>
+        </Container>
+    </Menu>
+);
 
 class Layout extends Component {
+    state = {};
+
+    hideFixedMenu = () => this.setState({ visible: false });
+    showFixedMenu = () => this.setState({ visible: true });
+
     render() {
+        const { visible } = this.state;
+
         return (
                 <div className="app-container">
-                        <header>
-                        </header>
+                        { visible ? <FixedMenu /> : null}
+
+
                         <main>
                         <BrowserRouter>
                             <Routes/>
